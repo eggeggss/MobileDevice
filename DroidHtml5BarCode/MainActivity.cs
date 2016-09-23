8a6bc7ed5bd2bada44e154fdc2563ffe83a4c35c
R700 <R700@R700-PC>
2016-09-24 02:27:15 +0800

    2

--- DroidHtml5BarCode/MainActivity.cs
+++ DroidHtml5BarCode/MainActivity.cs
@@ -10,7 +10,7 @@ namespace DroidHtml5BarCode
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
+            /*test*/
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
         }
